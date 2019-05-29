e2d826d30146f84a33831157e7681233fd02c353
Malte Schmidt <malte.schmidt@rwth-aachen.de>
2019-05-29 23:45:46 +1000

    Implemented Task D. Tests *Wall* and test case 9 still fail with index out of bounds.

--- pathFinder/DijkstraPathFinder.java
+++ pathFinder/DijkstraPathFinder.java
@@ -25,8 +25,13 @@ public class DijkstraPathFinder implements PathFinder
             PathCoordinate[][] pathCells = findPathsSingleSource(sourceCoord);
             for (int i=0; i<destLength; i++) {
                 System.out.println("i: " + i + ", j: " + j);
+                List<Coordinate> list;
                 Coordinate destCoord = map.destCells.get(i);
-                List<Coordinate> list = createShortestPathList(pathCells, sourceCoord, destCoord);
+                if (map.waypointCells.size() == 0) {
+                    list = createShortestPathList(pathCells, sourceCoord, destCoord);
+                } else {
+                    list = createShortestPathAlongWaypoints(sourceCoord, destCoord, map.waypointCells);
+                }
                 ShortestPath shortestPath = new ShortestPath(list);
                 System.out.println("shortestPath weight for " + j + ", " + i + ": " + shortestPath.getWeight());
                 paths.add(shortestPath);
@@ -36,6 +41,100 @@ public class DijkstraPathFinder implements PathFinder
         return path.coordList;
     }
 
+    // TODO: Method currently assumes that source, the waypoints, and the destination are different coordinates!
+    /**
+     * Create the shortest path from source to destination including all waypoints. Finding a solution is implemented
+     * as a dynamic program. First, we create a graph connecting each source/waypoint/destination cell with each other.
+     * The weight of each edge is the distance between the two vertex coordinates. The Graph class is used to create
+     * the graph representation. After we created this fully connected bidirectional graph a modified traveling
+     * salesman problem results. This traveling salesman problem is solved with the dynamic programming approach
+     * suggested by Held and Karp.
+     * @param sourceCoord The source coordinate of the path.
+     * @param destCoord The destination coordinate of the path.
+     * @param waypointCells All waypoint cells which should be visited.
+     * @return The shortest path from source to destination including all waypoints.
+     */
+    private List<Coordinate> createShortestPathAlongWaypoints(Coordinate sourceCoord,
+                                                              Coordinate destCoord, List<Coordinate> waypointCells) {
+        List<Coordinate> allCoords = new ArrayList<>(waypointCells);
+        allCoords.add(sourceCoord);
+        allCoords.add(destCoord);
+        Graph<Coordinate, ShortestPath> distanceGraph = new Graph<>();
+        // Create the graph representation between each source/waypoint/edge:
+        for (Coordinate startCoord : allCoords) {
+            PathCoordinate[][] pathCells = findPathsSingleSource(startCoord);
+            // Adding of startCoord to distance Graph is done by innermost loop too (as startCoord and coordB iterate over same coords)
+            for (Coordinate endCoord : allCoords) {
+                if (!distanceGraph.vertexExists(endCoord)) {
+                    // Add missing coordinate to graph as new vertex:
+                    distanceGraph.addVertex(endCoord);
+                }
+                // Exclude direct source <-> destination connections:
+                boolean sourceToDest = startCoord.equals(sourceCoord) && endCoord.equals(destCoord);
+                boolean destToSource = startCoord.equals(destCoord) && endCoord.equals(sourceCoord);
+                if (sourceToDest || destToSource) {
+                    // Don't add connections from source to destination or destination to source because at least one
+                    // waypoint has to be visited. This means the solution is not a direct connection between source
+                    // and destination.
+                    continue;
+                } // End of source <-> destination exclusion
+                // Add the edge between startCoord and endCoord if it does not exist already and startCoord != endCoord:
+                if (!startCoord.equals(endCoord) && !distanceGraph.edgeExists(startCoord, endCoord)) {
+                    List<Coordinate> path = createShortestPathList(pathCells, startCoord, endCoord);
+                    ShortestPath shortestPath = new ShortestPath(path);
+                    distanceGraph.addEdge(startCoord, endCoord, shortestPath.getWeight(), shortestPath);
+                }
+            }
+        } // Creation of shortest distances graph complete
+
+        ShortestPath result = searchForShortestPathAlongWaypoints(sourceCoord, destCoord, new HashSet<>(waypointCells), distanceGraph);
+        return result.coordList;
+    }
+
+    /**
+     * Dynamic programming approach for the search of the shortest path from a source to a destination including the
+     * waypoints. This method works recursively and the problem size is reduced by one vertex each recursion call.
+     *
+     * @param source The current source coordinate.
+     * @param dest The final destination coordinate.
+     * @param waypoints The original set is not changed. It is worked on a shallow copy of the set.
+     * @param graph The graph containing the information about the shortest paths between the vertices.
+     * @return The shortest path from source to destination including all waypoints.
+     */
+    private ShortestPath searchForShortestPathAlongWaypoints(Coordinate source, Coordinate dest,
+                                                                 HashSet<Coordinate> waypoints,
+                                                                 Graph<Coordinate, ShortestPath> graph) {
+        // Stop condition
+        if (waypoints.size() == 0) {
+            return graph.getEdgeInfo(source, dest);
+        }
+        // Recursive creation of candidates:
+        List<ShortestPath> canidates = new ArrayList<>();
+        for (Coordinate waypoint : waypoints) {
+            HashSet<Coordinate> newWaypoints = new HashSet<>(waypoints);
+            newWaypoints.remove(waypoint);
+            ShortestPath fromWaypoint = searchForShortestPathAlongWaypoints(waypoint, dest, newWaypoints, graph);
+            ShortestPath toWaypoint = graph.getEdgeInfo(source, waypoint);
+            ShortestPath newCandidate;
+            if (fromWaypoint.isValidPath() && toWaypoint.isValidPath()) {
+                // Both paths alone are valid, try to concatenate them:
+                newCandidate = new ShortestPath(toWaypoint, fromWaypoint);
+            } else {
+                // Create invalid dummy:
+                newCandidate = new ShortestPath();
+            }
+            if (newCandidate.isValidPath()) {
+                canidates.add(newCandidate);
+            }
+        }
+        if (canidates.size() == 0) {
+            // No valid paths were found, create a shortest path dummy with a weight of Integer.MAX_VALUE
+            return new ShortestPath();
+        } else {
+            // Comparison of Candidates:
+            return Collections.min(canidates, Comparator.comparing(ShortestPath::getWeight));
+        }
+    }
     /**
      * Dijkstra algorithm for the shortest paths starting from a single source. 
      * @return The shortest path from source to destination found.
@@ -166,10 +265,49 @@ public class DijkstraPathFinder implements PathFinder
 class ShortestPath {
     List<Coordinate> coordList;
     int pathWeight;
+    boolean validPath;
 
     public ShortestPath(List<Coordinate> coordList) {
         this.coordList = coordList;
         setWeight();
+        validPath = true;
+    }
+
+    /**
+     * A dummy path which is not valid will be created. The weight is Integer.MAX_VALUE and the coordList is empty.
+     */
+    public ShortestPath() {
+        this.coordList = new ArrayList<>();
+        this.pathWeight = Integer.MAX_VALUE;
+        this.validPath = false;
+    }
+    /**
+     * Constructor which constructs a shortest path which is a concatenation of the from and to paths. It checks if
+     * a valid path results from the concatenation (i.e. the last coordinate of the start path is the same as the first
+     * coordinate of the end path). Otherwise it sets the validPath flag to false, the coordList will be empty and the
+     * weight will be Integer.MAX_VALUE.
+     * @param start Path starts here.
+     * @param end Path ends here.
+     */
+    public ShortestPath(ShortestPath start, ShortestPath end) {
+        int startLastIdx = start.coordList.size() - 1;
+        Coordinate lastStart = start.coordList.get(startLastIdx);
+        Coordinate firstEnd = end.coordList.get(0);
+        if (!lastStart.equals(firstEnd)) {
+            // Path is not valid
+            coordList = new ArrayList<>();
+            validPath = false;
+            pathWeight = Integer.MAX_VALUE;
+        } else {
+            this.coordList = new ArrayList<>(start.coordList);
+            // Do not add point connecting the paths twice:
+            Coordinate connection = start.coordList.get(startLastIdx);
+            this.coordList.remove(startLastIdx);
+
+            this.coordList.addAll(end.coordList);
+            this.pathWeight = start.pathWeight + end.pathWeight - connection.getTerrainCost();
+            this.validPath = true;
+        }
     }
 
     public void setWeight() {
@@ -184,6 +322,16 @@ class ShortestPath {
         } else {
             this.pathWeight = incrementingWeight;
         }
+    }
+
+    /**
+     * It is possible that a path is not valid if the constructor concatenating
+     * two paths is used to create an instance and the last element of the start path and the first element of the
+     * end path are not equal.
+     * @return True if the path is valid.
+     */
+    public boolean isValidPath() {
+        return validPath;
     }
 
     public int getWeight() {
