59f147a33f60179fe3be1ed7549906ea63e10c26
Malte Schmidt <malte.schmidt@rwth-aachen.de>
2019-05-29 23:44:13 +1000

    Graph is now unidirectional.

--- pathFinder/Graph.java
+++ pathFinder/Graph.java
@@ -3,17 +3,17 @@ package pathFinder;
 import java.util.HashMap;
 
 /**
- * Implementation of a bidirectional weighted graph using the adjacency list representation. Alongside the
+ * Implementation of a unidirectional weighted graph using the adjacency list representation. Alongside the
  * weight an additional object (storing information) can be associated with an edge. The distinction of the vertex
  * objects is hash based.
  */
-public class BiGraph<T_Vertex, T_Info> {
+public class Graph<T_Vertex, T_Info> {
     private HashMap<T_Vertex, HashMap<T_Vertex, EdgeData>> adjacencyList;
 
     /**
      * Constructor.
      */
-    public BiGraph() {
+    public Graph() {
         adjacencyList = new HashMap<>();
     }
 
@@ -30,64 +30,84 @@ public class BiGraph<T_Vertex, T_Info> {
     }
 
     /**
-     * Add a bidirectional edge to the graph. The edge must not exist.
-     * @param vertA One endpoint of the edge.
-     * @param vertB Other endpoint of the edge.
+     * Checks if the vertex exists in the graph.
+     * @param vertex The vertex to be checked.
+     * @return True if the vertex already exists in the graph.
+     */
+    public boolean vertexExists(T_Vertex vertex) {
+        return adjacencyList.containsKey(vertex);
+    }
+
+    /**
+     * Checks if the edge exists in the graph.
+     * @param from Start vertex of edge.
+     * @param to End vertex of edge.
+     * @return True if the edge exists.
+     */
+    public boolean edgeExists(T_Vertex from, T_Vertex to) {
+        if (!adjacencyList.containsKey(from) || !adjacencyList.containsKey(to)) {
+            return false;
+        }
+        return adjacencyList.get(from).containsKey(to);
+    }
+    /**
+     * Add a directed edge to the graph. The edge must not exist.
+     * @param from Start vertex of edge.
+     * @param to End vertex of edge.
      * @param weight Weight of the edge.
      * @param info Additional information associated with the edge.
      * @throws IllegalArgumentException if one of the vertices does not exist or the edge already exists in the graph.
      */
-    public void addBiEdge(T_Vertex vertA, T_Vertex vertB, int weight, T_Info info) {
-        if (!adjacencyList.containsKey(vertA) || !adjacencyList.containsKey(vertB)) {
+    public void addEdge(T_Vertex from, T_Vertex to, int weight, T_Info info) {
+        if (!adjacencyList.containsKey(from) || !adjacencyList.containsKey(to)) {
             throw new IllegalArgumentException("One of the vertices does not exist in the graph.");
         }
-        if (adjacencyList.get(vertA).get(vertB) != null) {
+        if (edgeExists(from, to)) {
             throw new IllegalArgumentException("The edge already exists in the graph.");
         }
         EdgeData edgeData = new EdgeData(weight, info);
-        adjacencyList.get(vertA).put(vertB, edgeData);
-        adjacencyList.get(vertB).put(vertA, edgeData);
+        adjacencyList.get(from).put(to, edgeData);
     }
 
     /**
      * Method throws an exception if the edge does not exist.
-     * @param vertA One endpoint of the edge.
-     * @param vertB Other endpoint of the edge.
+     * @param from Start vertex of edge.
+     * @param to End vertex of edge.
      * @throws IllegalArgumentException if one of the vertices or the edge does not exist in the graph.
      */
-    private void checkEdgeExistence(T_Vertex vertA, T_Vertex vertB) {
-        if (!adjacencyList.containsKey(vertA) || !adjacencyList.containsKey(vertB)) {
+    private void checkEdgeExistence(T_Vertex from, T_Vertex to) {
+        if (!adjacencyList.containsKey(from) || !adjacencyList.containsKey(to)) {
             throw new IllegalArgumentException("One of the vertices does not exist in the graph.");
         }
-        if (adjacencyList.get(vertA).get(vertB) == null) {
+        if (!edgeExists(from, to)) {
             throw new IllegalArgumentException("The edge does not exists in the graph.");
         }
     }
 
     /**
      * Get the information associated with the edge.
-     * @param vertA One endpoint of the edge.
-     * @param vertB Other endpoint of the edge.
+     * @param from Start vertex of edge.
+     * @param to End vertex of edge.
      * @return The information associated with the edge.
      * @throws IllegalArgumentException if one of the vertices or the edge does not exist in the graph.
      */
-    public T_Info getEdgeInfo(T_Vertex vertA, T_Vertex vertB) {
-        checkEdgeExistence(vertA, vertB);
+    public T_Info getEdgeInfo(T_Vertex from, T_Vertex to) {
+        checkEdgeExistence(from, to);
 
-        return adjacencyList.get(vertA).get(vertB).getInfo();
+        return adjacencyList.get(from).get(to).getInfo();
     }
 
     /**
      * Get the edge weight.
-     * @param vertA One endpoint of the edge.
-     * @param vertB Other endpoint of the edge.
+     * @param from Start vertex of edge.
+     * @param to End vertex of edge.
      * @return The edge weight.
      * @throws IllegalArgumentException if one of the vertices or the edge does not exist in the graph.
      */
-    public int getEdgeWeight(T_Vertex vertA, T_Vertex vertB) {
-        checkEdgeExistence(vertA, vertB);
+    public int getEdgeWeight(T_Vertex from, T_Vertex to) {
+        checkEdgeExistence(from, to);
 
-        return adjacencyList.get(vertA).get(vertB).getWeight();
+        return adjacencyList.get(from).get(to).getWeight();
     }
 
     /**
